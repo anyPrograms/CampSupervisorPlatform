0134c7be98fd9da44dc8764926b53a27e0296840
杨一帆 <yangyf5@asiainfo.com>
2019-03-25 16:48:52 +0800

    DAO层 待开发

---
+++ src/main/java/src/main/java/com/csp/dao/CounselorDao.java
@@ -0,0 +1,14 @@
+package src.main.java.com.csp.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import src.main.java.com.csp.entity.Student;
+
+public interface CounselorDao {
+
+
+
+}
---
+++ src/main/java/src/main/java/com/csp/dao/ElectiveDao.java
@@ -0,0 +1,13 @@
+package src.main.java.com.csp.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import src.main.java.com.csp.entity.Student;
+
+public interface ElectiveDao {
+
+
+}
