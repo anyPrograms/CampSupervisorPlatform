717022199967149304174bdf9d8c7586da992623
杨一帆 <yangyf5@asiainfo.com>
2019-03-25 17:44:06 +0800

    使用annotation直接在DAO层//MAPPER 进行sql开发 更加直观

--- src/main/java/src/main/java/com/csp/dao/StudentDao.java
+++ src/main/java/src/main/java/com/csp/dao/StudentDao.java
@@ -4,29 +4,25 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
-
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Update;
 import src.main.java.com.csp.entity.Student;
 
 public interface StudentDao {
 
-	/**
-	 * 通过ID查询学生
-	 * 
-	 * @param id
-	 * @return
-	 */
-	Student findStudentById(String id);
 
-	/**
-	 * 查询部分学生
-	 * 
-	 * @param offset
-	 *            查询起始位置
-	 * @param limit
-	 *            查询条数
-	 * @return
-	 */
-	List<Student> findStudents(@Param("offset") int offset, @Param("limit") int limit);
+	@Select("SELECT * FROM Student WHERE s_id = #{s_id};")
+	public Student findStudentById(@Param("s_id") String s_id);
 
+	@Select("SELECT * FROM Student ORDER BY s_id LIMIT #{offset}, #{limit};")
+	public List<Student> findStudents(@Param("offset") int offset, @Param("limit") int limit);
+
+	@Insert("INSERT INTO Student (s_id,s_name,s_bunk,s_status) VALUES (#{s_id},#{s_name},#{s_bunk},#{s_status});")
+	public void addStudent(@Param("s_id") String s_id,@Param("s_name") String s_name,@Param("s_bunk") String s_bunk,@Param("s_id") String s_status);
+	
+	@Delete("DELETE FROM Student WHERE s_id = #{s_id};")
+	public void deleteStudent(@Param("s_id") String s_id);
 
 }
