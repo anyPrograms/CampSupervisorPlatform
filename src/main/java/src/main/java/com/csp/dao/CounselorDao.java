1104df0fa45f040b0ae565205cf1c977ea6584cf
michael joe <michael_joe@outlook.com>
2019-03-28 22:36:40 +0800

    '1'

--- src/main/java/src/main/java/com/csp/dao/CounselorDao.java
+++ src/main/java/src/main/java/com/csp/dao/CounselorDao.java
@@ -21,7 +21,7 @@ public interface CounselorDao {
     public List<Counselor> findCounselors(@Param("offset") int offset,@Param("limit") int limit);
 
     @Insert("INSERT INTO Counselor (c_id,c_name,c_bunk,c_intro) VALUES (#{c_id},#{c_name},#{c_bunk},#{c_intro})")
-    public void addCounselor(@Param("c_id") String c_id,@Param("c_name") String c_name,@Param("c_bunk") String c_bunk,@Param("c_id") String c_intro);
+    public void addCounselor(@Param("c_id") String c_id,@Param("c_name") String c_name,@Param("c_bunk") String c_bunk,@Param("c_intro") String c_intro);
 
     @Delete("DELETE FROM Counselor WHERE c_id = #{c_id};")
     public void deleteCounselor(@Param("c_id") String c_id);
