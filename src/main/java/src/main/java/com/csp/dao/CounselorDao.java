0d0d8ddaefc63793750a4373f10944f82ad301cb
michael joe <michael_joe@outlook.com>
2019-03-28 22:11:03 +0800

    修改了部分错误

--- src/main/java/src/main/java/com/csp/dao/CounselorDao.java
+++ src/main/java/src/main/java/com/csp/dao/CounselorDao.java
@@ -14,13 +14,13 @@ import src.main.java.com.csp.entity.Student;
 public interface CounselorDao {
 
 
-    @Select("SELECT * FROM Student WHERE c_id = #{c_id};")
+    @Select("SELECT * FROM Counselor WHERE c_id = #{c_id};")
     public Counselor findCounselorById(@Param("c_id") String c_id);
 
-    @Select("SELECT * FROM Student ORDER BY c_id LIMIT #{offset},#{limit};")
+    @Select("SELECT * FROM Counselor ORDER BY c_id LIMIT #{offset},#{limit};")
     public List<Counselor> findCounselors(@Param("offset") int offset,@Param("limit") int limit);
 
-    @Insert("INSERT INTO Counselor (c_id,c_name,c_bunk,c_status) VALUES (#{c_id},#{c_name},#{c_bunk},#{c_intro})")
+    @Insert("INSERT INTO Counselor (c_id,c_name,c_bunk,c_intro) VALUES (#{c_id},#{c_name},#{c_bunk},#{c_intro})")
     public void addCounselor(@Param("c_id") String c_id,@Param("c_name") String c_name,@Param("c_bunk") String c_bunk,@Param("c_id") String c_intro);
 
     @Delete("DELETE FROM Counselor WHERE c_id = #{c_id};")
