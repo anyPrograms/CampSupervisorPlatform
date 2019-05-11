28260709a9f1617d0823c37d4d0df32a04e4810e
michael joe <michael_joe@outlook.com>
2019-05-11 22:24:30 +0800

    修改utf8至utf8mb4

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE `student` (
   `student_bunk` varchar(20) NOT NULL COMMENT '营员bunkName',
   `student_status` varchar(2) NOT NULL COMMENT '营员状态' DEFAULT '1' , /*0 未参加  1 正常参加  2 外出就医  3 意外退出*/
   PRIMARY KEY (`student_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='营员表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='营员表';
 
 /*  student_id 生成策略
  * 	身份 S
@@ -27,7 +27,7 @@ CREATE TABLE `counselor` (
   `counselor_bunk` varchar(20) NOT NULL COMMENT 'counselorbunk',
   `counselor_intro` varchar(100) NOT NULL COMMENT 'counselor介绍' ,
   PRIMARY KEY (`counselor_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='counselor表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='counselor表';
 
 /* counselor_id 生成策略
 	身份 C
@@ -46,7 +46,7 @@ CREATE TABLE `director` (
   `director_id` varchar(8) NOT NULL COMMENT 'directorID',
   `director_name` varchar(20) NOT NULL COMMENT 'director名称',
   PRIMARY KEY (`director_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='director表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='director表';
 
 /* director_id 生成策略
 	身份 D
@@ -61,7 +61,7 @@ CREATE TABLE `staff` (
   `staff_id` varchar(9) NOT NULL COMMENT 'staffID',
   `staff_name` varchar(20) NOT NULL COMMENT 'staff名称',
   PRIMARY KEY (`staff_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='staff表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='staff表';
 
 /* staff_id 生成策略
 	身份 ST
@@ -78,7 +78,7 @@ CREATE TABLE `bunk` (
   `bunk_name` varchar(20) NOT NULL COMMENT 'bunk名称',
   `bunk_vol` varchar(4) NOT NULL COMMENT 'bunk容量',
   PRIMARY KEY (`bunk_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='bunk表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='bunk表';
 
 /* bunk_id生成策略 时间戳	*/
 
@@ -90,7 +90,7 @@ CREATE TABLE `elective` (
   `elective_intro` varchar(100) NOT NULL COMMENT 'elective介绍'，
   
   PRIMARY KEY (`elective_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='elective表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1 COMMENT='elective表';
 
 /* elective_id 生成策略
   !此生成规则已废弃，目前使用时间戳
@@ -115,18 +115,17 @@ CREATE TABLE `elective_schedule` (
   `age_group` varchar(2) NOT NULL COMMENT '适用年龄段',
   `date` varchar(32) NOT NULL COMMENT '日期',
   `schedule_vol` varchar(6) NOT NULL COMMENT '容量',
-  `location_id` varchar(14) NOT NULL 'locationID'
+  `location_id` varchar(14) NOT NULL 'locationID',
   PRIMARY KEY (`schedule_id`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='elective_schedule表';
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='elective_schedule表';
 
 -- 创建location表
 DROP TABLE IF EXISTS `location`;
 CREATE TABLE `location` (
-  `location_id` varchar(14) NOT NULL AUTO_INCREMENT COMMENT 'locationID',
-  `location_name` varchar(20) NOT NULL COMMENT 'location名称'
-  
+  `location_id` varchar(14) NOT NULL COMMENT 'locationID',
+  `location_name` varchar(20) NOT NULL COMMENT 'location名称',
   PRIMARY KEY (`location_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 COMMENT='location表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='location表';
 
 -- 创建counselor_elective_map表
 /*
@@ -138,27 +137,27 @@ CREATE TABLE `counselor_elective_map`  (
   `elective_id` varchar(14) NOT NULL,
   `is_specialty` varchar(2) NOT NULL DEFAULT '0',
   PRIMARY KEY (`counselor_id`, `elective_id`) USING BTREE
-) ENGINE = InnoDB CHARSET = utf8 ROW_FORMAT = Dynamic COMMENT='counselor_elective_map表';
+) ENGINE = InnoDB CHARSET = utf8mb4 ROW_FORMAT = Dynamic COMMENT='counselor_elective_map表';
 
 DROP TABLE IF EXISTS `student_elective_schedule_map`;
 CREATE TABLE `student_elective_schedule_map`  (
   `student_id` varchar(10)  NOT NULL,
   `schedule_id` varchar(48) NOT NULL,
   PRIMARY KEY (`student_id`, `schedule_id`) USING BTREE
-) ENGINE = InnoDB CHARSET = utf8 ROW_FORMAT = Dynamic COMMENT='student_elective_schedule_map表';
+) ENGINE = InnoDB CHARSET = utf8mb4 ROW_FORMAT = Dynamic COMMENT='student_elective_schedule_map表';
 
 DROP TABLE IF EXISTS `counselor_bunk_map`;
 CREATE TABLE `counselor_bunk_map`  (
   `counselor_id` varchar(11)  NOT NULL,
   `bunk_id` varchar(14) NOT NULL,
   PRIMARY KEY (`counselor_id`, `bunk_id`) USING BTREE
-) ENGINE = InnoDB CHARSET = utf8 ROW_FORMAT = Dynamic COMMENT='counselor_bunk_map 表';
+) ENGINE = InnoDB CHARSET = utf8mb4 ROW_FORMAT = Dynamic COMMENT='counselor_bunk_map 表';
 
 DROP TABLE IF EXISTS `student_bunk_map`;
 CREATE TABLE `student_bunk_map`  (
   `student_id` varchar(10)  NOT NULL,
   `bunk_id` varchar(14) NOT NULL,
   PRIMARY KEY (`student_id`, `bunk_id`) USING BTREE
-) ENGINE = InnoDB CHARSET = utf8 ROW_FORMAT = Dynamic COMMENT='student_bunk_map 表';
+) ENGINE = InnoDB CHARSET = utf8mb4 ROW_FORMAT = Dynamic COMMENT='student_bunk_map 表';
 SET FOREIGN_KEY_CHECKS = 1;
 
